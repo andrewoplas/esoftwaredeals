74ed754858c5f51d2856851c0c8285a51f23d552
David Maranga <xdmaranga4@gmail.com>
2018-02-14 10:12:33 +0800

    Fix layouts.master file

--- resources/views/layouts/footer.blade.php
+++
@@ -1,5 +0,0 @@
-		<script src="../../../public/js/jquery/jquery-3.3.1.min.js"></script>
-	    <script src="../../../public/js/bootstrap/bootstrap.min.js"></script>
-	    @yield('jsfiles')
-	</body>
-</html>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,3 +12,12 @@
 	    @yield('cssfiles')
 	    <link href="../../../public/css/ampleadmin/default.css" id="theme" rel="stylesheet">
 	</head>
+	<body>
+		@yield('content')
+
+
+		<script src="../../../public/js/jquery/jquery-3.3.1.min.js"></script>
+	    <script src="../../../public/js/bootstrap/bootstrap.min.js"></script>
+		@yield('jsfiles')
+	</body>
+</html>
