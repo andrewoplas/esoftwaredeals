64c02589e883ed768394765b8f2410301722d56b
David Maranga <xdmaranga4@gmail.com>
2018-02-14 09:54:15 +0800

    Add Head and Footer Layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+		<script src="../../../public/js/jquery/jquery-3.3.1.min.js"></script>
+	    <script src="../../../public/js/bootstrap/bootstrap.min.js"></script>
+	    @yield('jsfiles')
+	</body>
+</html>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<meta name="description" content="">
+    	<meta name="author" content="">
+    	<title>@yield('title')</title>
+	    <link href="../../../public/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+	    <link href="../../../public/css/ampleadmin/animate.css" rel="stylesheet">
+	    @yield('cssfiles')
+	    <link href="../../../public/css/ampleadmin/default.css" id="theme" rel="stylesheet">
+	</head>
