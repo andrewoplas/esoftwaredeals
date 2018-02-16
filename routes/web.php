1b4044a2ac05516f42b6906ca1763023dc4fe198
Andrew Oplas <andrewmoplas@gmail.com>
2018-02-16 11:24:49 +0800

    Finish feature/addProduct

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,17 @@
 Route::get('/tango', 'SessionController@create');
 Route::post('/tango', 'SessionController@store');
 Route::get('/logout', 'SessionController@destroy');
+
+Route::get('/tango', function () {
+    return view('login');
+});
+
+Route::get('/products', 'ProductsController@index');
+Route::get('/products/edit/{product}', 'ProductsController@show');
+Route::get('/products/add', 'ProductsController@show');
+Route::post('/products/edit/{product}', 'ProductsController@store');
+Route::post('/products/add', 'ProductsController@store');
+
+Route::get('/categories', 'CategoryController@index');
+Route::post('/categories', 'CategoryController@store');
+
