d22e52197126ec3e4faafef3c37302b50da0f69f
Andrew Oplas <andrewmoplas@gmail.com>
2018-02-20 15:37:23 +0800

    Change links under Product tree

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,28 +1,114 @@
 <div class="navbar-default sidebar" role="navigation">
-    <div class="sidebar-nav slimscrollsidebar">
-        <div class="sidebar-head">
-            <h3><span class="fa-fw open-close" style="color:#97999f"><i class="ti-menu hidden-xs"></i><i class="ti-close visible-xs" ></i></span> <span class="hide-menu" style="color:#97999f">Navigation</span></h3> </div>
-        <ul class="nav" id="side-menu">
-            <li class="user-pro">
-                <a href="#" class="waves-effect"><img src="/images/users/varun.jpg" alt="user-img" class="img-circle"> <span class="hide-menu"> Steve Gection<span class="fa arrow"></span></span>
-                </a>
-                <ul class="nav nav-second-level collapse" aria-expanded="false" style="height: 0px;">
-                    <li><a href="javascript:void(0)"><i class="ti-user"></i> <span class="hide-menu">My Profile</span></a></li>
-                    <li><a href="javascript:void(0)"><i class="ti-wallet"></i> <span class="hide-menu">My Balance</span></a></li>
-                    <li><a href="javascript:void(0)"><i class="ti-email"></i> <span class="hide-menu">Inbox</span></a></li>
-                    <li><a href="javascript:void(0)"><i class="ti-settings"></i> <span class="hide-menu">Account Setting</span></a></li>
-                    <li><a href="javascript:void(0)"><i class="fa fa-power-off"></i> <span class="hide-menu">Logout</span></a></li>
-                </ul>
-            </li>
-            <li> <a href="index.html" class="waves-effect"><i class="mdi mdi-av-timer fa-fw" data-icon="v"></i> <span class="hide-menu"> Dashboard </span></a>
-            </li>
-            <li> <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-cart-outline fa-fw" data-icon="v"></i> <span class="hide-menu"> Product <span class="fa arrow"></span> </span></a>
-                <ul class="nav nav-second-level">
-                    <li> <a href="/products"><i class="fa-fw">P</i><span class="hide-menu">Products</span></a> </li>
-                    <li> <a href="/products/add"><i class="fa-fw">A</i><span class="hide-menu">Add</span></a> </li>
-                    <li> <a href="/categories"><i class="fa-fw">C</i><span class="hide-menu">Categories</span></a> </li>
-                </ul>
-            </li>
-        </ul>
+     <div class="sidebar-nav slimscrollsidebar">
+          <div class="sidebar-head">
+               <h3>
+                    <span class="fa-fw open-close" style="color:#97999f">
+                         <i class="ti-menu hidden-xs"></i>
+                         <i class="ti-close visible-xs" ></i>
+                    </span> 
+                    <span class="hide-menu" style="color:#97999f">
+                         Navigation
+                    </span>
+               </h3> 
+          </div>
+
+          <ul class="nav" id="side-menu">
+               <li class="user-pro">
+                    <a href="#" class="waves-effect">
+                         <img src="/images/users/varun.jpg" alt="user-img" class="img-circle"> 
+                         <span class="hide-menu"> 
+                              Steve Gection
+                              <span class="fa arrow"></span>
+                         </span>
+                    </a>
+                    <ul class="nav nav-second-level collapse" aria-expanded="false" style="height: 0px;">
+                         <li>
+                              <a href="javascript:void(0)">
+                                   <i class="ti-user"></i> 
+                                   <span class="hide-menu">
+                                        My Profile
+                                   </span>
+                              </a>
+                         </li>
+                         <li>
+                              <a href="javascript:void(0)">
+                                   <i class="ti-wallet"></i> 
+                                   <span class="hide-menu">
+                                        My Balance
+                                   </span>
+                              </a>
+                         </li>
+                         <li>
+                              <a href="javascript:void(0)">
+                                   <i class="ti-email"></i> 
+                                   <span class="hide-menu">
+                                        Inbox
+                                   </span>
+                              </a>
+                         </li>
+                         <li>
+                              <a href="javascript:void(0)">
+                                   <i class="ti-settings"></i> 
+                                   <span class="hide-menu">
+                                        Account Setting
+                                   </span>
+                              </a>
+                         </li>
+                         <li>
+                              <a href="javascript:void(0)">
+                                   <i class="fa fa-power-off"></i> 
+                                   <span class="hide-menu"> 
+                                        Logout
+                                   </span>
+                              </a>
+                         </li>
+                    </ul>
+               </li>
+
+               <li> 
+                    <a href="index.html" class="waves-effect">
+                         <i class="mdi mdi-av-timer fa-fw" data-icon="v"></i> 
+                         <span class="hide-menu"> 
+                              Dashboard 
+                         </span>
+                    </a>
+               </li>
+
+               <li> 
+                    <a href="javascript:void(0);" class="waves-effect">
+                         <i class="mdi mdi-cart-outline fa-fw" data-icon="v"></i> 
+                         <span class="hide-menu"> 
+                              Product 
+                              <span class="fa arrow"></span> 
+                         </span>
+                    </a>
+                    <ul class="nav nav-second-level">
+                         <li> 
+                              <a href="/tango/products">
+                                   <i class="fa-fw">P</i>
+                                   <span class="hide-menu">
+                                        Products
+                                   </span>
+                              </a> 
+                         </li>
+                         <li> 
+                              <a href="/tango/products/add">
+                                   <i class="fa-fw">A</i>
+                                   <span class="hide-menu">
+                                        Add
+                                   </span>
+                              </a> 
+                         </li>
+                         <li> 
+                              <a href="/tango/categories">
+                                   <i class="fa-fw">C</i>
+                                   <span class="hide-menu">
+                                        Categories
+                                   </span>
+                              </a> 
+                         </li>
+                    </ul>
+               </li>
+          </ul>
     </div>
 </div>
