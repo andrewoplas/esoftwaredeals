136051c31179535afa9132c68e923f7b4d6976a5
Andrew Oplas <andrewmoplas@gmail.com>
2018-02-20 15:27:44 +0800

    Fix route for Products

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ Route::get('/tango', [ 'as' => 'login', 'uses' => 'SessionController@create']);
 Route::post('/tango', 'SessionController@store');
 Route::get('/tango/logout', 'SessionController@destroy');
 
-Route::get('/products', 'ProductsController@index');
-Route::get('/products/edit/{product}', 'ProductsController@show');
-Route::get('/products/add', 'ProductsController@show');
-Route::post('/products/edit/{product}', 'ProductsController@update');
-Route::post('/products/add', 'ProductsController@store');
+Route::get('/tango/products', 'ProductController@index');
+Route::get('/tango/products/edit/{product}', 'ProductController@show');
+Route::get('/tango/products/add', 'ProductController@show');
+Route::post('/tango/products/edit/{product}', 'ProductController@update');
+Route::post('/tango/products/add', 'ProductController@store');
 
-Route::get('/categories', 'CategoryController@index');
-Route::post('/categories', 'CategoryController@store');
+Route::get('/tango/categories', 'CategoryController@index');
+Route::post('/tango/categories', 'CategoryController@store');
