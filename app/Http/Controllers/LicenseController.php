2cf10a547440aa0c991583b9fdc050ee8d71d93b
David Maranga <xdmaranga4@gmail.com>
2018-02-26 20:51:26 +0800

    Finish feature/bulkAddlicense

--- app/Http/Controllers/LicenseController.php
+++ app/Http/Controllers/LicenseController.php
@@ -10,7 +10,7 @@ class LicenseController extends Controller
 {
 	public function __construct()
 	{
-	    //$this->middleware('auth');
+	    $this->middleware('auth');
 	}
 
 	public function index()
--- resources/views/pages/add_license.blade.php
+++ resources/views/pages/add_license.blade.php
@@ -19,17 +19,11 @@ Licenses
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
                 <h4 class="page-title">License Add</h4>
             </div>
-            <div class="col-lg-9 col-md-8 col-sm-8 col-xs-12">
-                <ol class="breadcrumb">
-                    <li><a href="/tango/dashboard">Dashboard</a></li>
-                    <li><a href="/tango/licenses">Licenses</a></li>
-                    <li class="active">Add</li>
-                </ol>
-            </div>
         </div>
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-info">
+                    <div class="panel-heading">Add License</div>
                     <div class="panel-wrapper collapse in" aria-expanded="true">
                         <div class="panel-body">
                             <form method="POST" enctype="multipart/form-data">
@@ -87,7 +81,7 @@ Licenses
                                 </div>
                                 <hr>
                                 <div class="form-actions m-t-40">
-                                    <button type="submit" id="submit" class="btn btn-success"> <i class="fa fa-check"></i> Add</button>
+                                    <button type="submit" id="submit" class="btn btn-success pull-right"> <i class="fa fa-check"></i> Add</button>
                                 </div>
                             </form>
                         </div>
